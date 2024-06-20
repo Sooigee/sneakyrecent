964c935ff28aa321aab9f4047d01b0d463292489
adryd <me@adryd.com>
2024-06-20 18:13:31 -0400

    1.21???

--- src/main/java/com/adryd/sneaky/mixin/MixinServerHandshakeNetworkHandler.java
+++ src/main/java/com/adryd/sneaky/mixin/MixinServerHandshakeNetworkHandler.java
@@ -3,16 +3,14 @@ package com.adryd.sneaky.mixin;
 import com.adryd.sneaky.Config;
 import com.adryd.sneaky.IPList;
 import net.minecraft.network.ClientConnection;
-import net.minecraft.network.packet.c2s.handshake.HandshakeC2SPacket;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerHandshakeNetworkHandler;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ServerHandshakeNetworkHandler.class)
 public class MixinServerHandshakeNetworkHandler {
@@ -23,13 +21,20 @@ public class MixinServerHandshakeNetworkHandler {
 
     @Shadow
     @Final
+    private MinecraftServer server;
+
+    @Shadow
+    @Final
     private static Text IGNORING_STATUS_REQUEST_MESSAGE;
 
-    @Inject(method = "onHandshake", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/ClientConnection;setS2CPacketHandler(Lnet/minecraft/network/packet/c2s/handshake/ConnectionIntent;)V", ordinal = 1), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
-    private void acceptsQuery(HandshakeC2SPacket packet, CallbackInfo ci) {
-        if (Config.INSTANCE.getDisableAllPingsUntilLogin() && !IPList.INSTANCE.canPing(this.connection.getAddress())) {
-            this.connection.disconnect(IGNORING_STATUS_REQUEST_MESSAGE);
-            ci.cancel();
+    @Redirect(method = "onHandshake", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;acceptsStatusQuery()Z"))
+    private boolean acceptsQuery(MinecraftServer instance) {
+        if (this.server.acceptsStatusQuery()) {
+            if (Config.INSTANCE.getDisableAllPingsUntilLogin() && !IPList.INSTANCE.canPing(this.connection.getAddress())) {
+                return false;
+            }
+            return true;
         }
+        return false;
     }
 }
