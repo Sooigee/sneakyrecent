329a39812c8adb892865e7668b6575d43496a72b
adryd <me@adryd.com>
2024-08-24 22:54:13 -0400

    i hate everything

--- src/main/java/com/adryd/sneaky/mixin/MixinServerLoginNetworkHandler.java
+++ src/main/java/com/adryd/sneaky/mixin/MixinServerLoginNetworkHandler.java
@@ -30,7 +30,7 @@ class MixinServerLoginNetworkHandler {
     @Nullable
     private GameProfile profile;
 
-    @Inject(method = "tickVerify", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/ClientConnection;send(Lnet/minecraft/network/packet/Packet;Lnet/minecraft/network/PacketCallbacks;)V", shift = At.Shift.AFTER))
+    @Inject(method = "tickVerify", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/PlayerManager;disconnectDuplicateLogins(Lcom/mojang/authlib/GameProfile;)Z"))
     private void atSuccessfulJoin(CallbackInfo ci) {
         IPList.INSTANCE.addToIPList(this.connection.getAddress());
     }
