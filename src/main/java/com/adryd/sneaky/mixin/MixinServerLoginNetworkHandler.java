57bae2ada923218038b7c90769317a1d6763d4b0
adryd <me@adryd.com>
2024-08-24 22:46:58 -0400

    what the heck fabric api

--- src/main/java/com/adryd/sneaky/mixin/MixinServerLoginNetworkHandler.java
+++ src/main/java/com/adryd/sneaky/mixin/MixinServerLoginNetworkHandler.java
@@ -30,7 +30,7 @@ class MixinServerLoginNetworkHandler {
     @Nullable
     private GameProfile profile;
 
-    @Inject(method = "tickVerify", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/ClientConnection;send(Lnet/minecraft/network/packet/Packet;Lnet/minecraft/network/PacketCallbacks;)V", shift = At.Shift.AFTER))
+    @Inject(method = "tickVerify", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerLoginNetworkHandler;sendSuccessPacket(Lcom/mojang/authlib/GameProfile;)V", shift = At.Shift.AFTER))
     private void atSuccessfulJoin(CallbackInfo ci) {
         IPList.INSTANCE.addToIPList(this.connection.getAddress());
     }
